ba1cd91a8cb85af3e4fbee2e105081f7da9cfced
Rafael <rafael_oliveira@gmx.com>
2021-03-07 21:21:32 -0300

    Updated jokes and added axios

--- pages/jokes/index.vue
+++ pages/jokes/index.vue
@@ -5,8 +5,43 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  
+  data() {
+    return {
+      jokes: []
+    }
+  },
+  async created() {
+    const config = {
+      headers: {
+        "Accept": "application/json"
+      }
+    }
+
+    try {
+      const res = await axios.get("https://icanhazdadjoke.com/search", config);
+
+      this.jokes = res.data.results;
+
+    } catch (err) {
+      console.log(err);
+    }
+    
+  },
+  head() {
+    return {
+      title: "Dad Jokes",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for dad jokes"
+        }
+      ]
+    }
+  }
 }
 </script>
 
