6e08990a675382f8688d2f9ab7a3d49546cbf903
Rafael <rafael_oliveira@gmx.com>
2021-03-09 15:39:38 -0300

    Updated single joke page and api call

--- pages/jokes/_id/index.vue
+++ pages/jokes/_id/index.vue
@@ -1,11 +1,48 @@
 <template lang="en">
   <div>
-    {{$route.params.id}}
+    <nuxt-link to="/jokes">Back to jokes</nuxt-link>
+    <h2>{{joke}}</h2>
+    <hr />
+    <small>Joke id: {{$route.params.id}}</small>
   </div>
 </template>
 <script>
+import axios from "axios";
+
 export default {
-  
+  data() {
+    return {
+      joke: ""
+    }
+  },
+  async created() {
+    const config = {
+      headers: {
+        "Accept": "application/json"
+      }
+    }
+
+    try {
+      const res = await axios.get(`https://icanhazdadjoke.com/j/${this.$route.params.id}`, config);
+
+      this.joke = res.data.joke;
+
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  head() {
+    return {
+      title: this.joke,
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for dad jokes"
+        }
+      ]
+    }
+  }
 }
 </script>
 <style lang="en">
